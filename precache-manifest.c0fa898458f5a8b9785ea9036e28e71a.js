fa9630ccba3e376238b9e1cc3f04bb85d36b83df
idan <idanelbaz@gmail.com>
2019-09-23 17:04:12 +0300

    Updates

---
+++ precache-manifest.c0fa898458f5a8b9785ea9036e28e71a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a80097ab40b1ef58a1c6479c786e6b",
+    "url": "/Geother/index.html"
+  },
+  {
+    "revision": "a890e2941f1b7b95804d",
+    "url": "/Geother/static/css/2.81cb803e.chunk.css"
+  },
+  {
+    "revision": "bfedfed893fb9c935dc0",
+    "url": "/Geother/static/css/main.938a6e28.chunk.css"
+  },
+  {
+    "revision": "a890e2941f1b7b95804d",
+    "url": "/Geother/static/js/2.29c0703d.chunk.js"
+  },
+  {
+    "revision": "bfedfed893fb9c935dc0",
+    "url": "/Geother/static/js/main.240c9961.chunk.js"
+  },
+  {
+    "revision": "410cbdc43f4a76817b82",
+    "url": "/Geother/static/js/runtime~main.15150d2c.js"
+  },
+  {
+    "revision": "a80f8cfc4976c2464200e38bb71ecc30",
+    "url": "/Geother/static/media/cityBackground.a80f8cfc.jpg"
+  },
+  {
+    "revision": "87c9fe8fc50083dbc00b26f2361e75d8",
+    "url": "/Geother/static/media/cityBackground2.87c9fe8f.jpeg"
+  },
+  {
+    "revision": "6723bd49f51479d71ac278af631f0418",
+    "url": "/Geother/static/media/cityBackground3.6723bd49.jpeg"
+  },
+  {
+    "revision": "bf9666d1f6768fe0e82517cf02e35d5d",
+    "url": "/Geother/static/media/cityBackground4.bf9666d1.jpeg"
+  },
+  {
+    "revision": "07227aeb7fc8b406c0f3a565a9576bee",
+    "url": "/Geother/static/media/icon.07227aeb.png"
+  }
+]);
